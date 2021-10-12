a58a84b8a776e67c30edd709a14c01b1451e3dbc
Isaac Busee <58541303+isakabusee@users.noreply.github.com>
2021-10-11 23:10:21 -0700

    more callbacks

--- callbacks.js
+++ callbacks.js
@@ -1,7 +1,21 @@
-setInterval(function() {
-    console.log("Hello!")
-}, 1000);
+// setInterval(function() {
+//     console.log("Hello!")
+// }, 1000);
 
-setTimeout(function(){
-    console.log("Hey!")
-}, 1000);
+// setTimeout(function(){
+//     console.log("Hey!")
+// }, 1000);
+
+//Callback function can be named or annonymous
+
+function greeting(name) {
+    console.log(`Hello ${name}, welcome!`);
+}
+
+function introduction(firstName, lastName, callback) {
+    const fullName = `${firstName} ${lastName}`;
+
+    callback(fullName)
+}
+
+introduction('Isaac', 'Busee', greeting)
